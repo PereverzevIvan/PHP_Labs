02a2ef4849fa4b4e392d144f8b5d2478122c92b1
Ivan <peregh320@gmail.com>
2023-02-17 22:00:47 +0300

    Выполнено домашнее задание 2.1

---
+++ 1lab/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Переверзев Иван 221-321</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header__container container">
+            <a href="#/" class="header__logo">
+                <img src="logo.svg" alt="" class="header__image">
+            </a>
+            <h1>2.1.Домашняя работа: Hello, World!</h1>
+        </div>
+    </header>
+    <main class="main">
+        <div class="container">
+            <?php echo '<h1 align="center">Hello World!</h1>'; ?>
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="footer__container container">
+            <a href="#/" class="header__logo">
+                <img src="logo.svg" alt="" class="header__image">
+            </a>
+            <h2>It is FOOTER</h2>
+        </div>
+    </footer>
+</body>
+</html>
